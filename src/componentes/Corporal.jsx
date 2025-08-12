a4be513e3d553b8cb0cfe97d564d39aba4b5b9e8
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-08-12 20:25:30 -0300

    paginas

--- src/componentes/Corporal.jsx
+++ src/componentes/Corporal.jsx
@@ -4,70 +4,68 @@ function Corporal() {
     return (
         <>
             <h2 style="text-align: center; color: #bb8a50;">TRATAMENTOS CORPORAIS</h2>
-
-
             <div className="container mt-4">
                 <div className="row g-4 justify-content-center">
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/drenagemlinfatica.png' className="card-img-top" alt="Drenagem linfática"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Drenagem linfática</h5>
-                                </div>
+                            <img src='/imagens/drenagemlinfatica.png' className="card-img-top" alt="Drenagem linfática" />
+                            <div className="card-body">
+                                <h5 className="card-title">Drenagem linfática</h5>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/massagemdetox.png' className="card-img-top" alt="Massagem Detox"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Massagem Detox</h5>
-                                </div>
+                            <img src='/imagens/massagemdetox.png' className="card-img-top" alt="Massagem Detox" />
+                            <div className="card-body">
+                                <h5 className="card-title">Massagem Detox</h5>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/massagemrelaxante.png' className="card-img-top" alt="Massagem Relaxante"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Massagem Relaxante</h5>
-                                </div>
+                            <img src='/imagens/massagemrelaxante.png' className="card-img-top" alt="Massagem Relaxante" />
+                            <div className="card-body">
+                                <h5 className="card-title">Massagem Relaxante</h5>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/ventosaterapia.png' className="card-img-top" alt="Ventosaterapia"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Ventosaterapia</h5>
-                                </div>
+                            <img src='/imagens/ventosaterapia.png' className="card-img-top" alt="Ventosaterapia" />
+                            <div className="card-body">
+                                <h5 className="card-title">Ventosaterapia</h5>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/liberacaomiofacial.jpg' className="card-img-top" alt="Liberação Miofascial"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Liberação Miofascial</h5>
-                                </div>
+                            <img src='/imagens/liberacaomiofacial.jpg' className="card-img-top" alt="Liberação Miofascial" />
+                            <div className="card-body">
+                                <h5 className="card-title">Liberação Miofascial</h5>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/esfoliacao.png' className="card-img-top" alt="Esfoliação e Hidratação"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Esfoliação e Hidratação</h5>
-                                </div>
+                            <img src='/imagens/esfoliacao.png' className="card-img-top" alt="Esfoliação e Hidratação" />
+                            <div className="card-body">
+                                <h5 className="card-title">Esfoliação e Hidratação</h5>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-6 col-md-4 col-lg-2 d-flex">
                         <div className="card h-100 w-100">
-                            <img src='/imagens/banhodelua.jpg' className="card-img-top" alt="Banho de Lua"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Banho de Lua</h5>
-                                </div>
+                            <img src='/imagens/banhodelua.jpg' className="card-img-top" alt="Banho de Lua" />
+                            <div className="card-body">
+                                <h5 className="card-title">Banho de Lua</h5>
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ src/componentes/Epilacao.jsx
@@ -0,0 +1,96 @@
+import './Epilacao.css';
+
+function Epilacao() {
+    return (
+        <>
+            <h2 style="text-align: center; color: #bb8a50;">EPILAÇÃO</h2>
+            <div className="container mt-4">
+                <div className="row g-4 justify-content-center">
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/intima.jpg' className="card-img-top" alt="Íntima" />
+                            <div className="card-body">
+                                <h5 className="card-title">Íntima</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/axila.jpg' className="card-img-top" alt="Axila" />
+                            <div className="card-body">
+                                <h5 className="card-title">Axila</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/rosto.jpg' className="card-img-top" alt="Rosto" />
+                            <div className="card-body">
+                                <h5 className="card-title">Rosto</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/buco.jpg' className="card-img-top" alt="Buço" />
+                            <div className="card-body">
+                                <h5 className="card-title">Buço</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/abdomen.png' className="card-img-top" alt="Abdômen" />
+                            <div className="card-body">
+                                <h5 className="card-title">Abdômen</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/costas.jpg' className="card-img-top" alt="Costas" />
+                            <div className="card-body">
+                                <h5 className="card-title">Costa</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/gluteo.jpg' className="card-img-top" alt="Glúteo" />
+                            <div className="card-body">
+                                <h5 className="card-title">Glúteo</h5>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/perna.png' className="card-img-top" alt="Perna" />
+                            <div className="card-body">
+                                <h5 className="card-title">Perna</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-6 col-md-4 col-lg-2 d-flex">
+                        <div className="card h-100 w-100">
+                            <img src='/imagens/meiaperna.png' className="card-img-top" alt="Meia Perna" />
+                            <div className="card-body">
+                                <h5 className="card-title">Meia Perna</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Epilacao;
